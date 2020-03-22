d542c6dd5cececc688968c1fa3b7b7f722aaca22
EGimenez <eduardgimenez@yahoo.com>
2020-03-22 19:59:25 +0100

    Introduccion de N-Grams

--- TopicModelling/EBC_Modelling.py
+++ TopicModelling/EBC_Modelling.py
@@ -1,12 +1,44 @@
-import pandas as pd
-import os
 import re
 import glob
-import sys
+import spacy
+import nltk
+from wordcloud import WordCloud
+import pandas as pd
+
+docs = list()
+nlp = spacy.load("en_core_web_sm")
 
 
+def clean_html(line):
+    return re.sub('<.*>', '', line)
 
-paragrahs = list()
+
+def token_filter(token):
+    if token.pos_ in ['NUM', 'SYM', 'ADP', 'AUX', 'DET', 'INTJ', 'PRON', 'SCONJ', 'PART']:
+        return ''
+    elif token.pos_ == 'PROPN':
+        return ''
+    else:
+        if token.lemma_ == 'PRON':
+            print('eo')
+        return token.lemma_.lower()
+
+
+def get_docs(article):
+    # Eliminamos Q&A
+    article = article.split('Transcript of the questions asked')[0]
+    article = article.split('* * *')[0]
+
+    res = re.findall('<p>(.*)</p>', article)
+
+    docs = ''
+
+    for line in res:
+        doc = nlp(clean_html(line))
+        for token in doc:
+            docs = docs + ' ' + token_filter(token)
+
+    return [docs]
 
 # Read data into papers
 path = "../tutorial/ecb_data/*.html"
@@ -14,18 +46,36 @@ for filename in glob.glob(path):
     with open(filename, 'r', encoding="utf-8") as f:
         try:
             article = f.read()
-            res = re.findall('<p>(.*)</p>', article)
-            paragrahs = paragrahs + res
+            docs = docs + get_docs(article)
         except Exception as ex:
             print('eo')
 
-
-
-# Import the wordcloud library
-from wordcloud import WordCloud
-
 # Join the different processed titles together.
-long_string = ','.join(paragrahs)
+long_string = ','.join(docs)
+
+# N-Grams
+words = re.findall(r"[\w']+", long_string)
+words = [word for word in long_string.split(' ')]
+
+bigrams = nltk.collocations.BigramAssocMeasures()
+trigrams = nltk.collocations.TrigramAssocMeasures()
+
+bigramFinder = nltk.collocations.BigramCollocationFinder.from_words(words)
+trigramFinder = nltk.collocations.TrigramCollocationFinder.from_words(words)
+
+bigramFinder.apply_freq_filter(50)
+the_bigrams = list(bigramFinder.score_ngrams(bigrams.pmi))
+# bigramPMITable = pd.DataFrame(list(bigramFinder.score_ngrams(bigrams.pmi)), columns=['bigram', 'PMI']).sort_values(by='PMI', ascending=False)
+# bigramPMITable[:10]
+# for i in range(1, 200):
+#     print(bigramPMITable[i - 1:i])
+
+trigramFinder.apply_freq_filter(50)
+the_trigrams = list(trigramFinder.score_ngrams(trigrams.pmi))
+# trigramPMITable = pd.DataFrame(list(trigramFinder.score_ngrams(trigrams.pmi)), columns=['trigram','PMI']).sort_values(by='PMI', ascending=False)
+# trigramPMITable[:10]
+# for i in range(1, 200):
+#     print(trigramPMITable[i - 1:i])
 
 # Create a WordCloud object
 wordcloud = WordCloud(background_color="white", max_words=1000, contour_width=3, contour_color='steelblue')
@@ -74,7 +124,7 @@ def plot_10_most_common_words(count_data, count_vectorizer):
 count_vectorizer = CountVectorizer(stop_words='english')
 
 # Fit and transform the processed titles
-count_data = count_vectorizer.fit_transform(paragrahs)
+count_data = count_vectorizer.fit_transform(docs)
 
 # Visualise the 10 most common words
 plot_10_most_common_words(count_data, count_vectorizer)
